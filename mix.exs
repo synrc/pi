e09652a5dd1fe1425dd592185c8a6b45321021e5
Namdak Tonpa <maxim@synrc.com>
2019-08-08 05:05:21 +0300

    init

---
+++ mix.exs
@@ -0,0 +1,34 @@
+defmodule PI.Mixfile do
+  use Mix.Project
+
+  def project() do
+    [
+      app: :pi,
+      version: "0.8.0",
+      elixir: "~> 1.7",
+      description: "PI Process Instance",
+      package: package(),
+      deps: deps()
+    ]
+  end
+
+  def package do
+    [
+      files: ~w(doc src mix.exs LICENSE),
+      licenses: ["ISC"],
+      maintainers: ["Namdak Tonpa"],
+      name: :pi,
+      links: %{"GitHub" => "https://github.com/synrc/pi"}
+    ]
+  end
+
+  def application() do
+    [mod: {:pay, []}]
+  end
+
+  def deps() do
+    [
+      {:ex_doc, "~> 0.11", only: :dev}
+    ]
+  end
+end
